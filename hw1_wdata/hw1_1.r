f15f07ec47323b237500e137a64d0ad5094f0659
weichunliao <weichun5795@gmail.com>
2017-03-08 17:54:38 +0800

    hw1-1

--- hw1_wdata/hw1_1.r
+++ hw1_wdata/hw1_1.r
@@ -1,7 +1,11 @@
-gpredict = function(dftrain, dftest){
-	
+library("MASS")
+gpredict = function(dftrain, dftest = NULL){
+	dftrain_a = dftrain$life_post_consumer
+	mua = mean(dftrain_a)
 
-￼￼
+	feature_names = colnames(dftest)
+	dftrain_b = dftrain[c(feature_names)]
+	mub = colMeans(dftrain_b)
 
 }
 
@@ -11,14 +15,24 @@ dftest1 = read.csv("df1_test1.csv")
 dftest1y = read.csv("df1_test1y.csv")
 dftrain = dftrain[1:200,]
 
+k = ncol(dftrain-1) # number of feature varables (Xb)
+n = nrow((dftrain)) # number of training subjects
+
 dftrain_a = dftrain$life_post_consumer
 mua = mean(dftrain_a)
+
 dftrain_b = dftrain[ , -which(colnames(dftrain) %in% c("life_post_consumer"))]
 mub = colMeans(dftrain_b)
-s_ab = 
 
-# s_ab = matrix(0, nrow=1, ncol=nfeature)
-# s_bb=matrix(0, nrow=nfeature, ncol=nfeature)
+n_mub = matrix(data=1, nrow=n) %*% t(as.matrix(mub))
+s_ab = ((n-1)^(-1)) * t(as.matrix(dftrain_a-mua)) %*% as.matrix(dftrain_b-n_mub)
+s_bb = ((n-1)^(-1)) * t(as.matrix(dftrain_b-n_mub)) %*% as.matrix(dftrain_b-n_mub)
+
+n_test = nrow(dftest1)
+n_mub_test = matrix(data=1,  nrow = n_test) %*% t(as.matrix(mub))
+
+predict = mua + s_ab %*% ginv(s_bb) %*% t(as.matrix(dftest1 - n_mub_test))
+mae1a = mean(abs(dftest1y[,1] - predict))
 # predict=rep(0, ntest))
-￼
+
 
