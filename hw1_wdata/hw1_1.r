358530177cc400e5ea728925a96052204965e7bc
weichunliao <weichun5795@gmail.com>
2017-03-08 20:50:29 +0800

    hw1-1

--- hw1_wdata/hw1_1.r
+++ hw1_wdata/hw1_1.r
@@ -1,38 +1,54 @@
-library("MASS")
-gpredict = function(dftrain, dftest = NULL){
+gpredict = function(dftrain = NULL, dftest = NULL){
+	nfeature = ncol(dftest) # number of features
+	n = nrow(dftrain) # number of training observations
+	n_test = nrow(dftest) # the number of testing observations
+
+	if ( is.null(n) ) {
+		if (is.null(nfeature)) {
+			ret = list(mua = NULL, mub = NULL, s_ab = NULL, s_bb = NULL, predict = NULL)
+			return(ret)
+		}
+		ret=list(mua=0, mub=rep(0, nfeature),
+		s_ab = matrix(0, nrow=1, ncol=nfeature),
+		s_bb=matrix(0, nrow=nfeature, ncol=nfeature),
+		predict=rep(0, n_test))
+		return(ret)
+	} 
+
 	dftrain_a = dftrain$life_post_consumer
-	mua = mean(dftrain_a)
+	if (is.null(dftrain_a)) {
+		mua = 0
+	} else {
+		mua = mean(dftrain_a) # mean of Xa
+	}
 
-	feature_names = colnames(dftest)
-	dftrain_b = dftrain[c(feature_names)]
-	mub = colMeans(dftrain_b)
+	if ( is.null(nfeature) ) {
+		dftrain_b = dftrain[, -which(names(dftrain) %in% c("life_post_consumer"))]
+	} else {
+		feature_names = colnames(dftest)
+		dftrain_b = dftrain[c(feature_names)]
+	}
+	mub = colMeans(dftrain_b) # means of Xb
+	
+	mat_mub = matrix(data=1, nrow=n) %*% t(as.matrix(mub))
+	s_ab = ((n-1)^(-1)) * t(as.matrix(dftrain_a - mua)) %*% as.matrix(dftrain_b - mat_mub)
+	s_bb = ((n-1)^(-1)) * t(as.matrix(dftrain_b - mat_mub)) %*% as.matrix(dftrain_b - mat_mub)
 
+	if ( is.null(dftest) ) {
+		predict = NULL
+	} else {
+		mat_mub_test = matrix(data=1,  nrow = n_test) %*% t(as.matrix(mub))
+		predict = mua + s_ab %*% solve(s_bb) %*% t(as.matrix(dftest - mat_mub_test))
+		predict = as.vector(predict)
+	}
+	rep = list(mua = mua, mub = mub, s_ab= s_ab, s_bb=s_bb, predict = predict)
+	return(rep)
 }
 
-setwd("/Users/weichun/Desktop/rsand/hw1_wdata")
-dftrain = read.csv("df1_train.csv")
-dftest1 = read.csv("df1_test1.csv")
-dftest1y = read.csv("df1_test1y.csv")
-dftrain = dftrain[1:200,]
+# setwd("/Users/weichun/Desktop/rsand/hw1_wdata")
+# dftrain = read.csv("df1_train.csv")
+# dftest1 = read.csv("df1_test1.csv")
+# dftest1y = read.csv("df1_test1y.csv")
+# dftrain = dftrain[1:200,]
 
-k = ncol(dftrain-1) # number of feature varables (Xb)
-n = nrow((dftrain)) # number of training subjects
-
-dftrain_a = dftrain$life_post_consumer
-mua = mean(dftrain_a)
-
-dftrain_b = dftrain[ , -which(colnames(dftrain) %in% c("life_post_consumer"))]
-mub = colMeans(dftrain_b)
-
-n_mub = matrix(data=1, nrow=n) %*% t(as.matrix(mub))
-s_ab = ((n-1)^(-1)) * t(as.matrix(dftrain_a-mua)) %*% as.matrix(dftrain_b-n_mub)
-s_bb = ((n-1)^(-1)) * t(as.matrix(dftrain_b-n_mub)) %*% as.matrix(dftrain_b-n_mub)
-
-n_test = nrow(dftest1)
-n_mub_test = matrix(data=1,  nrow = n_test) %*% t(as.matrix(mub))
-
-predict = mua + s_ab %*% ginv(s_bb) %*% t(as.matrix(dftest1 - n_mub_test))
-mae1a = mean(abs(dftest1y[,1] - predict))
-# predict=rep(0, ntest))
-
-
+# out = gpredict(dftrain, dftest1)
