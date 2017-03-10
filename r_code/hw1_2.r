6d6a5a353247944455efac6eee974dc28869a157
weichunliao <weichun5795@gmail.com>
2017-03-10 18:31:54 +0800

    hw1-2

--- r_code/hw1_2.r
+++ r_code/hw1_2.r
@@ -1,4 +1,11 @@
-mle_update = function(mu, s, n, x){
+mle_update = function(mu_old, s_old, n_old, x){
+	n = n_old +1
+	mu = mu_old + (1/n) * (x - mu_old)
+	s = ((n_old/n) * s_old) + (1/n) * as.matrix(x-mu) %*% t(as.matrix(x-mu))
 
+	result = list(mu = mu, s = s, n = n)
+	return (result)
 }
+
+
 
