265d5b3ca615f3b8ff355c4fdf43a0a62ac25826
weichunliao <weichun5795@gmail.com>
2017-04-07 17:02:26 +0800

    hw2 2,3

--- r_code/hw2_2.r
+++ r_code/hw2_2.r
@@ -1,3 +1,4 @@
+# this function is used for counting t-value
 reg_tvalue = function(y, x) {
 	y = matrix(y, ncol = 1)
 	xmat = as.matrix(cbind(c(1), x))
@@ -14,42 +15,50 @@ gen_utagmat = function(utagvec = NULL, price) {
 	utag_count = sort(table(unlist(strsplit(utagvec, ','))), decreasing = T)
 
 	threshold = 5
-	utag_thresh5_idx = utag_count < threshold
-	utag_count_thresh5 = utag_count[!utag_thresh5_idx]
-	if (length(utag_count_thresh5) == 0) {
+	utag_low_thresh_idx = utag_count < threshold
+	# remove utag freq. below threshold
+	utag_count_low_thresh = utag_count[!utag_low_thresh_idx]
+	# check if there still remain some utag 
+	if (length(utag_count_low_thresh) == 0) {
 		const = matrix(c(1), nrow = length(price), ncol = 1)
 		colnames(const) = c("constant")
 		return (const)
 	}
-	dummy_mat = t(sapply(strsplit(utagvec, ','), function(x) as.numeric(names(utag_count_thresh5) %in% x)))
-	colnames(dummy_mat) = names(utag_count_thresh5)
+	# make dummy variable matrix after feature selection though utag freq.
+	dummy_mat = t(sapply(strsplit(utagvec, ','), function(x) as.numeric(names(utag_count_low_thresh) %in% x)))
+	colnames(dummy_mat) = names(utag_count_low_thresh)
+	# count t-value for each utag
 	dummy_tvalue = apply(dummy_mat, 2, function(x) reg_tvalue(price, x))
-	names(dummy_tvalue) = names(utag_count_thresh5)
+	names(dummy_tvalue) = names(utag_count_low_thresh)
 
+	# sort t-value for each utag
 	ordered_t_idx = order(abs(dummy_tvalue), decreasing = T)
 	ordered_dummy_tvalue = dummy_tvalue[ordered_t_idx]
 	tvalue_threshold = 1
-	tvalue_thresh1_idx = abs(ordered_dummy_tvalue) > tvalue_threshold
-	final_feature = ordered_dummy_tvalue[tvalue_thresh1_idx]
+	tvalue_thresh_idx = abs(ordered_dummy_tvalue) >= tvalue_threshold
+	# remove utag tvalue below threshold
+	final_feature = ordered_dummy_tvalue[tvalue_thresh_idx]
+	# check if there still remain some utag
 	if (length(final_feature) == 0) {
 		const = matrix(c(1), nrow = length(price), ncol = 1)
 		colnames(const) = c("constant")
 		return (const)
 	}
 
-	umat = dummy_mat[,c(names(final_feature))]
-	colnames(umat) = paste('user_', colnames(umat),sep='')
-	final_umat = cbind(constant = c(1), umat)
+	# select the dummy variable after feature selection though t-value and add constant col 
+	umat = cbind(constant = c(1), dummy_mat[,c(names(final_feature))])
+	# sort the umat by the abs(t-value)
+	colnames(umat)[2:ncol(umat)] = paste('user_', c(names(final_feature)),sep='')
 
-	return (final_umat)
+	return (umat)
 }
 ### using for test
-# load(file='rtb1_train.rdata')
+load(file='rtb1_train.rdata')
 
-# rtb1_train = rtb1_train[1:300,]
-# umat1 = gen_utagmat(rtb1_train$user_tags, rtb1_train$paying_price)
-# head(umat1)
-# y = rtb1_train$paying_price
-# w = solve(t(umat1) %*% umat1, t(umat1) %*% y)
+rtb1_train = rtb1_train[1:300,]
+umat1 = gen_utagmat(rtb1_train$user_tags, rtb1_train$paying_price)
+head(umat1)
+y = rtb1_train$paying_price
+w = solve(t(umat1) %*% umat1, t(umat1) %*% y)
 ###
 
--- r_code/hw2_3.r
+++ r_code/hw2_3.r
@@ -1,4 +1,100 @@
-gen_uagentmat = function(uagentvec, y) {
+reg_tvalue = function(y, x) {
+	y = matrix(y, ncol = 1)
+	xmat = as.matrix(cbind(c(1), x))
+	bhead = solve(t(xmat) %*% xmat, t(xmat) %*% y)
+	yhead = xmat %*% bhead
+	e1 = y - yhead
+	var1 = sum(e1 * e1) / (length(e1) - 2)
+	sigma2 = solve(t(xmat) %*% xmat) * var1
+	t1 = bhead[2]/sqrt(sigma2[2,2])
 
+	return (t1)
 }
+
+gen_uagentmat = function(uagentvec, price) {
+	N = length(uagentvec)
+	# define regex pattern
+	regex_pattern = "([A-Za-z][A-Za-z0-9]{1,})"
+	# string compare though regex pattern
+	uagent_list = regmatches(uagentvec, gregexpr(regex_pattern, uagentvec))
+	# keep only unique words in each row.
+	uagent_list = lapply(uagent_list, unique)
+
+	uagent_count = table(unlist(uagent_list))
+	# target_uagent = c("Mozilla", "Windows", "NT", "AppleWebKit", "KHTML", "like", "Gecko", "Maxthon", "Chrome", "Safari")
+	# uagent_count = uagent_count[target_uagent]
+	# remove doc freq. lower than lower threshold
+	lower_thresh = 10
+	uagent_count_low_thresh = uagent_count[uagent_count >= lower_thresh ]
+	# remove doc freq. higher than upper threshold
+	upper_thresh = N/2
+	uagent_count_final = uagent_count_low_thresh[uagent_count_low_thresh <= upper_thresh]
+	# check if there still have some feature
+	if (length(uagent_count_final) == 0) {
+		const = matrix(c(1), nrow = length(price), ncol = 1)
+		colnames(const) = c("constant")
+		return (const)
+	}
+	# make dummy variable matrix after feature selection though doc freq.
+	dummy_mat = t(sapply(uagent_list, function(x) as.numeric(names(uagent_count_final) %in% x)))
+	colnames(dummy_mat) = names(uagent_count_final)
+	# count t-value for each utag
+	dummy_tvalue = apply(dummy_mat, 2, function(x) reg_tvalue(price, x))
+	names(dummy_tvalue) = names(uagent_count_final)
+	# order t-value by alphabetical order
+	# dummy_tvalue = dummy_tvalue[order(names(dummy_tvalue))]
+	dummy_tvalue = dummy_tvalue[order(names(dummy_tvalue), decreasing = T)]
+
+	# sort t-value for each utag
+	ordered_t_idx = order(abs(dummy_tvalue), decreasing = T)
+	ordered_dummy_tvalue = dummy_tvalue[ordered_t_idx]
+	tvalue_threshold = 1
+	tvalue_thresh_idx = abs(ordered_dummy_tvalue) >= tvalue_threshold
+	# remove utag tvalue below threshold
+	final_feature = ordered_dummy_tvalue[tvalue_thresh_idx]
+	# check if there still remain some utag
+	if (length(final_feature) == 0) {
+		const = matrix(c(1), nrow = length(price), ncol = 1)
+		colnames(const) = c("constant")
+		return (const)
+	}
+
+	# select the dummy variable after feature selection though t-value and add constant col 
+	umat = cbind(constant = c(1), dummy_mat[,c(names(final_feature))])
+	# sort the umat by the abs(t-value)
+	colnames(umat)[2:ncol(umat)] = paste('agent_', c(names(final_feature)),sep='')
+
+	return (umat)
+}
+
+###using for test
+# load(file='rtb1_train.rdata')
+# rtb1_train = rtb1_train[1:1500,]
+# y = rtb1_train$paying_price
+# umat1 = gen_uagentmat(rtb1_train$user_agent,y)
+
+# print(head(umat1))
+# print(head(sort(colSums(umat1), decreasing=TRUE), n=10))
+#  #remove linearly independent columns
+#  qr1 = qr(umat1, tol =1e-7)
+#  ind3 = qr1$pivot[1:qr1$rank]
+#  rank0 = ncol(umat1)
+#  if(qr1$rank < rank0) {
+# 	cat("There are", rank0, "columns, but rank is only", qr1$rank,"\n")
+# 	toremove = qr1$pivot[(qr1$rank+1):rank0]
+
+# 	cat("list of features removed", toremove,"\n")
+
+# 	tokeep = qr1$pivot[1:qr1$rank]
+# 	umat1 = umat1[,tokeep]
+# }
+# w = solve(t(umat1) %*% umat1, t(umat1) %*% y)
+# print(w)
+# ###
+# uagentvec = rtb1_train$user_agent
+# price = rtb1_train$paying_price
+
+
+
+
 
