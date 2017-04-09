9d01e4191002c75c1efd28c45410893f7858f5e2
Weichun <weichun5795@gmail.com>
2017-04-10 01:02:10 +0800

    hw2-1

--- r_code/hw2_1.r
+++ r_code/hw2_1.r
@@ -1,22 +1,36 @@
 lm_evmax = function(y, xmat) {
 
+	N = nrow(xmat)
+	M = ncol(xmat)
+	lambda = 0.001*N
+	w = solve( lambda*diag(M) + t(xmat)%*%xmat ) %*% t(xmat) %*% y
+	e0 = y - xmat%*%w
+	beta = as.numeric(N/(t(e0)%*%e0))
+	alpha = as.numeric(lambda*beta)
+
+	repeat{
+		A = alpha*diag(M) + beta*t(xmat)%*%xmat
+		mN =  beta*solve(A)%*%t(xmat)%*%y
+		eigenvaules_A = eigen(beta*t(xmat)%*%xmat)$values ####
+
+		gamma =	sum(eigenvaules_A /(alpha+eigenvaules_A))
+
+		alpha_new = as.numeric(gamma/(t(mN)%*%mN))
+		e1 = y - xmat%*%mN
+		beta_new = as.numeric((N-gamma)/(t(e1)%*%e1))
+
+		if( (abs(alpha - alpha_new) + abs(beta - beta_new)) < 10^(-5) ){
+			break
+		}
+		else {
+			alpha = alpha_new
+			beta = beta_new
+		}
+	}
+
+	ans = list(mN = mN, mNsd = sqrt(diag(solve(A))), alpha = alpha, beta=beta)
+	return (ans)
 }
-
-
-setwd('/home/chun/Desktop/rsand/r_code')
-load(file='rtb2_train.rdata')
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 ### test if correct
