65b7bf14b05d3052af604b9db64eb131dc8558f7
weichunliao <weichun5795@gmail.com>
2017-04-09 15:27:30 +0800

    hw2-1

---
+++ r_code/hw2_1.r
@@ -0,0 +1,31 @@
+lm_evmax = function(y, xmat) {
+
+}
+
+
+setwd('/home/chun/Desktop/rsand/r_code')
+load(file='rtb2_train.rdata')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+### test if correct
+# nfeat=20
+# rtb3 = rtb2_train[1:(nfeat+1)]
+# y=as.matrix(rtb3[,1])
+# xmat = model.matrix(paying_price~., data=rtb3)
+# lmev1 = lm_evmax(y, xmat)
+# lmev1
+
+
+
