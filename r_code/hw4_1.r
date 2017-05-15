5892b19683c238fb9855a1765ce3ca1f5063b552
Weichun <weichun5795@gmail.com>
2017-05-15 15:25:45 +0800

    hw4

---
+++ r_code/hw4_1.r
@@ -0,0 +1,44 @@
+filter_chisq = function(dstrain, ypos="pos", min_count=5, chi_threshold = 10^(-5)){
+
+
+
+
+
+
+	if () {
+		return ( list(colpos = NULL, colname = NULL, chistat = NULL) )
+	}
+	result = list(colpos = , colname = , chistat = )
+	return (result)
+}
+
+
+
+
+
+#############
+load('hw4ds1.rdata')
+testfold = 1
+dstrain1 = hw4ds1[-folds[[testfold]],] 
+######
+
+chi_test = function(product, feature) {
+	f1c1=sum()
+	f0c1=
+	f1c0=
+	f0c0=
+}
+
+
+
+feature_mat = dstrain1[,-1]
+chi_test_value = apply(feature_mat, 2, )
+
+
+
+#########
+
+out1=filter_chisq(dstrain1)
+
+
+
