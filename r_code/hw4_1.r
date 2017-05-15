6d0afdb2c0c8ee3d5d9914182c0df53705f12233
weichunliao <weichun5795@gmail.com>
2017-05-15 17:12:36 +0800

    hw4

--- r_code/hw4_1.r
+++ r_code/hw4_1.r
@@ -31,22 +31,23 @@ chi_threshold = 10^(-5)
 chi_test = function(product, feature) {
 	ppp = as.numeric( product == ypos )
 	feature = as.numeric( feature > 0 )
-	cat(sum(feature), "\n")
-	if (sum(feature) <= min_count ) {
-		return (NULL)
-	}
-	cat("gggg")
 
+	if (sum(feature) <= min_count ) {
+		return (NA)
+	}
 
 	chi_value = chisq.test(ppp, feature, correct=FALSE, simulate.p.value = TRUE)
 
-	return (chi_value$statistic)
+	return (as.numeric(chi_value$statistic))
 }
 
 
 
 feature_mat = dstrain1[,-1]
-chi_test_values = apply(feature_mat, 2, function(x) chi_test(dstrain1[,1], x) )
+chi_test_values = unlist(apply(feature_mat, 2, function(x) chi_test(dstrain1[,1], x) ))
+chi_test_values[which(chi_test_values < chi_threshold, arr.ind=T )] = NA
+names(sort(chi_test_values, decreasing=T, na.last=NA))
+
 
 
 
