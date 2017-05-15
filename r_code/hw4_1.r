d1df2339bf49b9f88eb0bd0f88e9b8fab8f22ef5
weichunliao <weichun5795@gmail.com>
2017-05-15 16:58:44 +0800

    hw4

--- r_code/hw4_1.r
+++ r_code/hw4_1.r
@@ -20,19 +20,33 @@ filter_chisq = function(dstrain, ypos="pos", min_count=5, chi_threshold = 10^(-5
 load('hw4ds1.rdata')
 testfold = 1
 dstrain1 = hw4ds1[-folds[[testfold]],] 
+
+
+ypos="pos"
+min_count=5
+chi_threshold = 10^(-5)
 ######
 
+
 chi_test = function(product, feature) {
-	f1c1=sum()
-	f0c1=
-	f1c0=
-	f0c0=
+	ppp = as.numeric( product == ypos )
+	feature = as.numeric( feature > 0 )
+	cat(sum(feature), "\n")
+	if (sum(feature) <= min_count ) {
+		return (NULL)
+	}
+	cat("gggg")
+
+
+	chi_value = chisq.test(ppp, feature, correct=FALSE, simulate.p.value = TRUE)
+
+	return (chi_value$statistic)
 }
 
 
 
 feature_mat = dstrain1[,-1]
-chi_test_value = apply(feature_mat, 2, )
+chi_test_values = apply(feature_mat, 2, function(x) chi_test(dstrain1[,1], x) )
 
 
 
